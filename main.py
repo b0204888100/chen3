9ccd31fceff29c0710c68bd475c1b655d619510a
b0204888100 <b0204888100@gmail.com>
2017-05-23 14:00:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
-INSTANCE_NAME = 'instance-1-f1fa5f96'
+INSTANCE_NAME = 'instance-1'
 INSTANCE_ZONE = 'asia-east1-a'
 PROJECT = 'esoteric-cider-165206'
 
